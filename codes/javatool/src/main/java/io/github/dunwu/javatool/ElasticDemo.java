bb0cd3699b611e65caee55cb5c0d2047dd4afd74
Zhang Peng <forbreak@163.com>
2017-12-27 19:50:12 +0800

    :sparkles: 添加 log4j 接入 elk 示例

--- codes/javatool/src/main/java/io/github/dunwu/javatool/ElasticDemo.java
+++ codes/javatool/src/main/java/io/github/dunwu/javatool/ElasticDemo.java
@@ -8,19 +8,33 @@ import org.slf4j.LoggerFactory;
 
 /**
  * 向 Elastic 日志中心传输日志
- * logstash-logback-encoder jar 包会根据 logback 中的配置，将日志数据定向传输到 logstash
- * 详见 src/main/resources/logback.xml appender 配置
- * 使用 udp 方式传输时，有丢失日志的情况（ELK-UDP）
- * 使用 tcp 方式传输时，不会丢失日志（ELK-TCP）
+ * Logback
+ * logstash-logback-encoder jar 包会根据 logback 中的配置，将日志数据定向传输到 logstash 详见
+ * src/main/resources/logback.xml appender 配置 使用 udp 方式传输时，有丢失日志的情况（ELK-UDP） 使用 tcp 方式传输时，不会丢失日志（ELK-TCP）
+ * Log4j
+ * 通过 org.apache.log4j.net.SocketAppender 发送 TCP 数据，Logstash 服务器使用 log4j input 插件 接收
  * @author Zhang Peng
  */
 public class ElasticDemo {
     private static final Logger logger = LoggerFactory.getLogger(ElasticDemo.class);
+    private static final org.apache.log4j.Logger log4jLog = org.apache.log4j.Logger.getLogger(ElasticDemo.class);
     private static volatile int index = 0;
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(100);
-        for (int i = 0; i < 10000; i++) {
+    private static void sendLog4jLog() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    log4jLog.info(String.format("这是第 %d 条日志", ++index));
+                }
+            });
+        }
+    }
+
+    private static void sendLogbackLog() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
@@ -28,5 +42,10 @@ public class ElasticDemo {
                 }
             });
         }
+    }
+
+    public static void main(String[] args) {
+        sendLog4jLog();
+//        sendLogbackLog();
     }
 }
