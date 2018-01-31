1009796a4a557125c886f36b7d790bc5784eb97a
Zhang Peng <forbreak@163.com>
2018-01-31 11:32:25 +0800

    :ok_hand: 更新嵌入式Tomcat启动类

--- codes/javatool/server/README.md
+++ codes/javatool/server/README.md
@@ -7,12 +7,12 @@
 
 **版本**
 
-- JDK：1.8
-- Tomcat：8.5.24
+* JDK：1.8
+* Tomcat：8.5.24
 
 ## Tomcat
 
-### API 启动嵌入式 Tomcat
+### Windows 启动
 
 执行 `io.github.dunwu.javatool.server.SimpleTomcatServer#main` 方法。
 
@@ -23,6 +23,27 @@
 ### 插件启动嵌入式 Tomcat
 
 由于插件很久没有更新（最新版本发布时间：2013-11-11），目前只能找到 Tomcat6 、Tomcat7 插件，所以弃用。
+
+### 脚本启动
+
+> 本项目添加了脚本启动范例。
+>
+> 脚本代码全在 [`scripts`](https://github.com/dunwu/java-stack/tree/master/scripts) 目录下。
+
+* 初始化
+
+```sh
+wget https://raw.githubusercontent.com/dunwu/java-stack/master/scripts/init.sh
+chmod 777 init.sh
+./init.sh
+```
+
+* 发布
+
+```
+cd /home/zp/source/java-stack/scripts
+./javatool-server-release.sh master develop
+```
 
 ## Jetty
 
---
+++ codes/javatool/server/src/main/java/io/github/dunwu/javatool/server/SimpleTomcatServer.java
@@ -0,0 +1,38 @@
+package io.github.dunwu.javatool.server;
+
+import java.util.Optional;
+
+import org.apache.catalina.startup.Tomcat;
+
+/**
+ * 简单的嵌入式 Tomcat 启动类
+ * 启动后可访问 http://localhost:8080/javatool-server/
+ * @author Zhang Peng
+ */
+public class SimpleTomcatServer {
+    private static final int PORT = 8080;
+    private static final String CONTEXT_PATH = "/javatool-server";
+
+    public static void main(String[] args) throws Exception {
+        // 设定 profile
+        Optional<String> profile = Optional.ofNullable(System.getProperty("spring.profiles.active"));
+        System.setProperty("spring.profiles.active", profile.orElse("develop"));
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(PORT);
+        tomcat.getHost().setAppBase(".");
+        tomcat.addWebapp(CONTEXT_PATH, getAbsolutePath() + "src/main/webapp");
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    private static String getAbsolutePath() {
+        String path = null;
+        String folderPath = SimpleTomcatServer.class.getProtectionDomain().getCodeSource().getLocation().getPath()
+                .substring(1);
+        if (folderPath.indexOf("target") > 0) {
+            path = folderPath.substring(0, folderPath.indexOf("target"));
+        }
+        return path;
+    }
+}
