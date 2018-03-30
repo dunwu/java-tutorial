123a087170322a56a39f92cbca4c1d151139510a
Zhang Peng <forbreak@163.com>
2018-03-30 16:39:27 +0800

    :bookmark: log4j2 示例

---
+++ codes/javalib/log/log4j2-demo/src/main/java/io/github/dunwu/javalib/log/Log4j2Demo.java
@@ -0,0 +1,25 @@
+package io.github.dunwu.javalib.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Logback 示例
+ * @author Zhang Peng
+ * @date 2018/3/29
+ * @see <a href="https://logback.qos.ch/">logback 官网</a>
+ */
+public class Log4j2Demo {
+
+    private static final Logger logger = LoggerFactory.getLogger(Log4j2Demo.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            logger.trace("NO.{} 这是一条 {} 日志记录", i, "trace");
+            logger.debug("NO.{} 这是一条 {} 日志记录", i, "debug");
+            logger.info("NO.{} 这是一条 {} 日志记录", i, "info");
+            logger.warn("NO.{} 这是一条 {} 日志记录", i, "warn");
+            logger.error("NO.{} 这是一条 {} 日志记录", i, "error");
+        }
+    }
+}
