6898a13a9861d1ced81b83aea3b8967e956f9615
Zhang Peng <forbreak@163.com>
2018-03-30 14:17:11 +0800

    :bookmark: log4j 示例

---
+++ codes/javalib/log/log4j-demo/src/main/java/io/github/dunwu/javalib/log/Log4jDemo.java
@@ -0,0 +1,25 @@
+package io.github.dunwu.javalib.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * log4j 示例
+ * @author Zhang Peng
+ * @date 2018/3/29
+ * @see <a href="http://logging.apache.org/log4j/1.2/">log4j 官网</a>
+ */
+public class Log4jDemo {
+
+    private static final Logger logger = LoggerFactory.getLogger(Log4jDemo.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            logger.trace("NO.{} 这是一条 {} 日志记录", i, "trace");
+            logger.debug("NO.{} 这是一条 {} 日志记录", i, "debug");
+            logger.info("NO.{} 这是一条 {} 日志记录", i, "info");
+            logger.warn("NO.{} 这是一条 {} 日志记录", i, "warn");
+            logger.error("NO.{} 这是一条 {} 日志记录", i, "error");
+        }
+    }
+}
