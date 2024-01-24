8934acd897acac8b61c0e66a423aea7c0c6d4fe9
dunwu <forbreak@163.com>
2024-01-24 23:04:53 +0800

    build: 修改构建方式

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,23 +24,24 @@ cd ${ROOT_DIR}/docs/.temp
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-if [[ ${GITHUB_TOKEN} && ${GITEE_TOKEN} ]]; then
+#if [[ ${GITHUB_TOKEN} && ${GITEE_TOKEN} ]]; then
+if [[ ${GITHUB_TOKEN} ]]; then
   msg='自动部署'
   GITHUB_URL=https://dunwu:${GITHUB_TOKEN}@github.com/dunwu/java-tutorial.git
-  GITEE_URL=https://turnon:${GITEE_TOKEN}@gitee.com/turnon/java-tutorial.git
+#  GITEE_URL=https://turnon:${GITEE_TOKEN}@gitee.com/turnon/java-tutorial.git
   git config --global user.name "dunwu"
   git config --global user.email "forbreak@163.com"
 else
   msg='手动部署'
   GITHUB_URL=git@github.com:dunwu/java-tutorial.git
-  GITEE_URL=git@gitee.com:turnon/java-tutorial.git
+#  GITEE_URL=git@gitee.com:turnon/java-tutorial.git
 fi
 git init
 git add -A
 git commit -m "${msg}"
 # 推送到github gh-pages分支
 git push -f "${GITHUB_URL}" master:gh-pages
-git push -f "${GITEE_URL}" master:gh-pages
+#git push -f "${GITEE_URL}" master:gh-pages
 
 cd -
 rm -rf ${ROOT_DIR}/docs/.temp
